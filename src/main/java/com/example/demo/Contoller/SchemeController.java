6e6b301e1c6210dad793f27a4a93b338eccc5fc1
Marcus <marcusxq11@gmail.com>
2018-12-16 00:21:03 +0100

    Navbar opdateret + kalender html + statistik html

--- src/main/java/com/example/demo/Contoller/SchemeController.java
+++ src/main/java/com/example/demo/Contoller/SchemeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class schemeController {
+public class SchemeController {
 
 @Autowired
     SchemeRepo schemeRepo;
