45729c2256b1f3146534149345b8d4709bd54b7b
MD <altair2400@gmail.com>
2018-12-15 22:52:13 +0100

    kalender funktion opret

---
+++ src/main/java/com/example/demo/Contoller/schemeController.java
@@ -0,0 +1,57 @@
+package com.example.demo.Contoller;
+
+import com.example.demo.Model.Repository.SchemeRepo;
+import com.example.demo.Model.Scheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class schemeController {
+
+@Autowired
+    SchemeRepo schemeRepo;
+
+
+
+@GetMapping("/kalender")
+    public String calender(Model model){
+
+    model.addAttribute("schema",new Scheme());
+
+
+    return "schema";
+}
+
+@PostMapping("/kalender")
+    public String calender(Scheme scheme){
+    schemeRepo.save(scheme);
+
+    return "redirect:/kalender";
+}
+ @GetMapping("/redigerKalender")
+ public String redigerKalender(Model model, @RequestParam(value = "id") Long id){
+    model.addAttribute("schema",schemeRepo.findById(id));
+
+
+
+
+    return "editSchema";
+ }
+
+@PostMapping("/redigerKalender")
+public String redigerKalender() {
+
+
+
+
+    return "redirect:/index";
+}
+
+
+
+
+}
---
+++ src/main/java/com/example/demo/Model/Repository/SchemeRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo.Model.Repository;
+
+import com.example.demo.Model.Scheme;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SchemeRepo extends CrudRepository<Scheme,Long> {
+
+
+}
